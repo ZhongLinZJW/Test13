27475e84870f09903433980eb9bc0b20f7503705
MS <3050119305@qq.com>
2018-10-17 16:21:36 +0800

    firstcommit

---
+++ Text13/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Text13
+//
+//  Created by apple on 2018/10/17.
+//  Copyright © 2018年 ms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Text13/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Text13
+//
+//  Created by apple on 2018/10/17.
+//  Copyright © 2018年 ms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
